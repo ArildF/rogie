0f1d340e3c3ca9beb11daec8f122b44bd31088ba
Arild <Arild@265f36a7-d215-7648-b499-c2f1fa04e3c0>
2005-04-23 20:12:13 +0000

    * \Display.py 	(TimeDecorator) - A decorator that prints out the current time 	(Display) - Applied TimeDecorator to all methods.
    git-svn-id: svn://localhost@63 265f36a7-d215-7648-b499-c2f1fa04e3c0

--- trunk/Display.py
+++ trunk/Display.py
@@ -3,24 +3,40 @@
 
 #is used to display output
 
+import time
+
+def TimeDecorator():
+    def decorator(f):
+        def _wrapper(*args, **kwargs):
+            print time.strftime( "%H:%M:%S>" ), 
+            return f(*args, **kwargs)
+        return _wrapper
+    return decorator
+
 class Display:
     instance = None
     
+    @TimeDecorator()
     def userSpeech( self, nick, statement ):
         print nick + ": " + statement
 
+    @TimeDecorator()
     def userPm( self, nick, statement ):
         print "PM from " + nick + ": " + statement
 
+    @TimeDecorator()
     def pmTo( self, nick, statement ):
         print "To " + nick + " in PM: " + statement
 
+    @TimeDecorator()
     def userJoin( self, room, nick ):
         print nick + " joined " + room
 
+    @TimeDecorator()
     def userLeave( self, room, nick ):
         print nick + " left " + room
 
+    @TimeDecorator()
     def listUsers( self, room, desc, users ):
         print "In " + room + ":\n" + desc
         for user in users[ :-1]:
